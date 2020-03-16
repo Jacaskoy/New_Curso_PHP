e54897321f56eaf821b67d9c9b73b7a0db018d42
jacaskoy <48741438+jacaskoy2019@users.noreply.github.com>
2020-03-16 15:33:44 -0500

    Modularizando Contenidos

---
+++ blog-php/vistas/paginas/modulos/banner.php
@@ -0,0 +1,73 @@
+<!--=====================================
+BANNER
+======================================-->
+
+<div class="bannerEstatico"></div>
+
+<section class="jd-slider fade-slider">
+	
+	<div class="slide-inner">
+		
+		<ul class="slide-area">
+			
+			<li>
+				
+				<img src="img/banner01.jpg" class="img-fluid">
+
+			</li>
+
+			<li>
+				
+				<img src="img/banner02.jpg" class="img-fluid">
+
+			</li>
+
+			<li>
+				
+				<img src="img/banner03.jpg" class="img-fluid">
+
+			</li>
+
+			<li>
+				
+				<img src="img/banner04.jpg" class="img-fluid">
+
+			</li>
+
+		</ul>
+
+	</div>
+
+	<div class="controller d-none">
+		
+		<a class="auto" href="#">
+			
+			<i class="fas fa-play fa-xs"></i>
+			<i class="fas fa-pause fa-xs"></i>
+
+		</a>
+
+		<div class="indicate-area"></div>
+
+	</div>
+
+</section>
+
+<!--=====================================
+BUSCADOR PARA MÓVIL
+======================================-->
+
+<div class="container-fluid d-block d-md-none py-3 bg-white">
+	
+	<div class="input-group input-group-sm">
+
+		<input type="text" class="form-control" placeholder="Buscar">
+
+		<div class="input-group-append">
+
+			<span class="input-group-text"><i class="fas fa-search"></i></span>
+
+		</div>
+	</div>
+
+</div>
